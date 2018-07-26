1e7bb7530818ca72a2bc174da1b7c86902b1cc7e
gonasapudihima <41733986+gonasapudihima@users.noreply.github.com>
2018-07-26 16:41:27 +0530

    Create 1.java

---
+++ bigen/sample.java/1.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ import java.util.Scanner;
+ class Postive_Negative 
+{
+    public static void main(String[] args) 
+    {
+        int i=3;
+      
+        if(i> 3)
+        {
+            System.out.println("i is Positive");
+        }
+        else if(i < 3)
+        {
+            System.out.println("i is Negative");
+        }
+        else
+        {
+            System.out.println(" zero");
+        }
+    }
+}
+ 
