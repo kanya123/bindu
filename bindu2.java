bd750b15aba2567ddf23b8fdd50c7283abbb426e
gonasapudihima <41733986+gonasapudihima@users.noreply.github.com>
2018-07-31 10:11:56 +0530

    Create bindu2.java

---
+++ bindu2.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i=3;
+      
+        if(i> 3)
+        {
+            System.out.println("i is Positive");
+        }
+        else if(i < 3)
+        {
+            System.out.println("i is Negative");
+        }
+        else
+        {
+            System.out.println(" zero");
+        }
+		// your code goes here
+	}
+}
