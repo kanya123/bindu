20225fb3414977e06303f59e42c8f1c0014a85f5
gonasapudihima <41733986+gonasapudihima@users.noreply.github.com>
2018-08-09 10:33:40 +0530

    Create 22.java

---
+++ 22.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i,big,n;
+		Scanner sc=new Scanner (System.in);
+		n=sc.nextInt();
+		int a[]=new int[n];
+		for(i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+			
+		}
+		big=a[0];
+		for(i=1;i<n;i++)
+		{
+			if(a[i]>big)
+			{
+				big=a[i];
+			}
+		}
+		System.out.print(big);
+		}
+		// your code goes here
+	}
